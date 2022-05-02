32bdda7f98dba473185b4f6008b0382834a5dde8
Mikkel Yde Engquist <mikk9553@edu.easj.dk>
2022-05-02 14:23:17 +0200

    UI test

---
+++ UITests/UITests/UnitTest1.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+namespace UITests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static readonly string DriverDirectory = "C:\\Users\\syv22\\OneDrive\\Dokumenter\\1.Datamatiker_ting\\Web_drivers";
+
+
+        private static IWebDriver _driver;
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _driver = new ChromeDriver(DriverDirectory); // fast
+            //_driver = new FirefoxDriver(DriverDirectory);  // slow
+            //_driver = new EdgeDriver(DriverDirectory); // now working
+
+        }
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestMethod]
+        public void TestGetAllExcuses()
+        {
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+            string url = "file:C:\\Users\\syv22\\OneDrive\\Dokumenter\\1.Datamatiker_ting\\3.SemesteV2\\EksamensprojektHtml\\RestExcusesHtml-master\\index.html";
+            _driver.Navigate().GoToUrl(url);
+
+            IWebElement buttonElement = _driver.FindElement(By.Id("getButton"));
+            buttonElement.Click();
+            //Thread.Sleep(2000);
+            //IWebElement excuseList = _driver.FindElement(By.Id("getAllExcuses"));
+            
+            IWebElement excuseList = wait.Until(d => d.FindElement(By.Id("getAllExcuses")));
+            string text = excuseList.Text;
+            Assert.IsTrue(text.Contains("3"));
+
+        }
+    }
+}
